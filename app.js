95a916aaa2bc2968e07b8512cdac0b3215bcbda1
Charan <sangaticherry3306@gmail.com>
2025-04-09 00:28:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+
+app.get("/",(req,res)=>{
+ res.send("Hello")
+})
+
+module.exports = app
